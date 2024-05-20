ebc530f1911d5ffb1d5e551126c7f3841c7b3619
文浩宇 <wenhaoyu@mgtv.com>
2024-05-20 16:06:03 +0800

    qwen

--- qwen7b_pre.py
+++ qwen7b_pre.py
@@ -1,13 +1,12 @@
 from modelscope import AutoModelForCausalLM, AutoTokenizer
 import pandas as pd
 
-device = "cuda:1" # the device to load the model onto
+device1 = "cuda:1" # the device to load the model onto
 
 test_a = pd.read_csv('test_a.csv',encoding='gbk')
 
 model = AutoModelForCausalLM.from_pretrained(
-    "/home/wenhy/Qwen1.5-7b-chat/qwen/Qwen1___5-7B-Chat",
-    device_map="auto"
+    "/home/wenhy/Qwen1.5-7b-chat/qwen/Qwen1___5-7B-Chat",device = device1
 )
 tokenizer = AutoTokenizer.from_pretrained("/home/wenhy/Qwen1.5-7b-chat/qwen/Qwen1___5-7B-Chat")
 
