103f444ac356f2622b9e6eb2bf0b9e205d7945f1
文浩宇 <wenhaoyu@mgtv.com>
2024-05-20 16:34:35 +0800

    qwen

--- qwen7b_pre.py
+++ qwen7b_pre.py
@@ -11,12 +11,12 @@ device = "cuda" # the device to load the model onto
 
 
 model = AutoModelForCausalLM.from_pretrained(
-    "/home/wenhy/Qwen1.5-7b-chat/qwen/Qwen1___5-7B-Chat",
+    "./root/.cache/modelscope/hub/qwen/",
     torch_dtype="auto",
     device_map="auto"
 )
 
-tokenizer = AutoTokenizer.from_pretrained("/home/wenhy/Qwen1.5-7b-chat/qwen/Qwen1___5-7B-Chat")
+tokenizer = AutoTokenizer.from_pretrained("./root/.cache/modelscope/hub/qwen/")
 
 def prompt1(x,y,z):
     #prompt = "Give me a short introduction to large language model."
